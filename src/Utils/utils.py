404bfe3f16923927445f6388472dfc5cf82e0309
mrzaizai2k <chibao24.12.1999@gmail.com>
2024-06-09 21:03:15 +0700

    remove files

---
+++ src/Utils/utils.py
@@ -0,0 +1,10 @@
+import yaml 
+
+def read_text_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            text = file.read()
+        return text
+    except FileNotFoundError:
+        print(f"File not found at {file_path}")
+        return None
