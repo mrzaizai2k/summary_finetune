340ec3fda710694b096e4eb49134c24788a64576
mrzaizai2k <chibao24.12.1999@gmail.com>
2024-06-10 21:46:25 +0700

    resume traing with beam on 5000 data - not good

--- README.md
+++ README.md
@@ -6,7 +6,27 @@ I'm supposed to finetune Vietnamese news summarization for [Falconsai/text_summa
         unzip 'master.zip'
         
 
-### Datasets
+## Datasets
 - [Wikilingua](https://github.com/esdurmus/Wikilingua)
 - [Vietnews](https://github.com/ThanhChinhBK/vietnews)
 - [Pho_NER](https://github.com/VinAIResearch/PhoNER_COVID19)
+
+## Model
+The model can be found here: https://huggingface.co/chibao24/vietnamese_mt5_summary_model
+
+## Performance
+5 text - chibao24/vietnamese_mt5_summary_model
+- use_cahe=True
+- CPU w/o batch: 10.352751731872559
+- GPU w/o batch: 5.818488597869873
+- CPU: 4.4028120040893555
+- GPU: 2.0009677410125732
+- GPU + bfloat16: 2.177988052368164 "same generated text"
+- GPU + INT8: 7.794734239578247 "same generated text"
+
+10 text - chibao24/vietnamese_mt5_summary_model
+- GPU + bfloat16: 2.879676580429077 "same generated text" 
+
+10 text - VietAI/vit5-large-vietnews-summarization
+- GPU: 6.69340443611145
+- GPU + bfloat16: 2.2307209968566895
--- src/example.py
+++ src/example.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append("")
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import time
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoConfig
 from src.Utils.utils import read_text_from_file
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -9,298 +10,41 @@ print(f"Using {device} device")
 
 model_dir="vietnamese_mt5_summary_model"
 tokenizer = AutoTokenizer.from_pretrained(model_dir)  
-model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
-model.to(device)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_dir, 
+                                              torch_dtype=torch.bfloat16,).to(device)
+config = AutoConfig.from_pretrained(model_dir)
+print("config", config)
+print("generation_config", model.generation_config)
+
 
 file_path = "data/sample.txt"
 text = read_text_from_file(file_path)
+txt_list = 10*[text] 
 
-encoding = tokenizer(text, return_tensors="pt")
+start_time = time.time()
+encoding = tokenizer(txt_list, return_tensors="pt")
 input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
+
 outputs = model.generate(
     input_ids=input_ids, attention_mask=attention_masks,
     max_length=256,
-    num_beams=5,
-    no_repeat_ngram_size=2,
-    early_stopping=True,
-    # do_sample=True,
+    # num_beams=4,
+    # no_repeat_ngram_size=3,
+    # early_stopping=True,
+    # length_penalty= 2.0,
+    do_sample=True,
     # top_p=0.8,
-    # top_k=0,
+    penalty_alpha = 0.6,
+    top_k=4,
     # temperature=0.5,
 )
+end_time = time.time()
+processing_time = end_time - start_time
+print(f"Processing time: {processing_time:.4f} seconds")
+
 for output in outputs:
     line = tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
     print(line)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
